6f62e1f2c01b3bf239e759017522f30fb2f9439d
zhangyingwei <zhangyw_001@126.com>
2016-09-01 22:20:23 +0800

    app

--- src/test/java/com/zhangyingwei/ikeywords/App.java
+++ src/test/java/com/zhangyingwei/ikeywords/App.java
@@ -1,33 +1,28 @@
 package com.zhangyingwei.ikeywords;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import org.ansj.app.keyword.KeyWordComputer;
 import org.ansj.app.keyword.Keyword;
-import org.ansj.domain.Result;
-import org.ansj.domain.Term;
-import org.ansj.recognition.impl.NatureRecognition;
-import org.ansj.splitWord.Analysis;
-import org.ansj.splitWord.analysis.BaseAnalysis;
 
+import com.zhangyingwei.ikeywords.keyword.KeyWordComputer;
 import com.zhangyingwei.rssreader.client.RssClient;
 import com.zhangyingwei.rssreader.model.RssEntity;
-import com.zhangyw.utils.String.StringUtil;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-//		String url = "https://luolei.org/rss/";
-		String url = "http://localhost:9000/rss/rss3.xml";
+		String url = "http://blog.zhangyingwei.com/atom.xml";
 		List<RssEntity> rss = new RssClient().buildRssEntitys(url);
-		System.out.println(rss.get(0).getContent());
-		for(RssEntity e:rss){
-			KeyWordComputer computer = new KeyWordComputer(30);
-			List<Keyword> list = computer.computeArticleTfidf(e.getTitle(), e.getContent());
-			System.out.println(e.getTitle());
-			System.out.println(list);
-			System.out.println("-----------");
-		}
+		System.out.println(rss.size());
+		KeyWordComputer computer = new KeyWordComputer(30);
+		List<Keyword> list = computer.computeArticleTfidf(rss.get(3).getTitle(), rss.get(3).getContent());
+		System.out.println(rss.get(3).getTitle());
+		System.out.println(list);
+		System.out.println("-----------");
+//		for(RssEntity e:rss){
+//			KeyWordComputer computer = new KeyWordComputer(30);
+//			List<Keyword> list = computer.computeArticleTfidf(e.getTitle(), e.getContent());
+//			System.out.println(e.getTitle());
+//			System.out.println(list);
+//			System.out.println("-----------");
+//		}
 	}
 }
